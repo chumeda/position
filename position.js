d3fb44d0d0dfe43a77e4a00d5ac798a936ece888
Corynne <chumeda@hawaii.edu>
2015-10-05 23:33:22 -1000

    Create git ignore file

--- position.js
+++ position.js
@@ -1,15 +1,25 @@
+/**
+ * accepts a sorted array of integers and a target value and
+ * returns the index where the target value is found
+ * or if the target value is not found,
+ * returns the index where it would be if it were inserted in order
+ * @param sortedlist - the array of integers for initializing function
+ * @param target - target value to be found
+ * @returns {*}
+ */
+
 
 function position(sortedlist, target) {
-    for(i=0; i<sortedlist.length; i++) {
-        if(sortedlist[i] === target) {
-            return i;
-        }
-        else if(sortedlist[i] > target) {
-            return i--;
-        }
-
+  for(var i=0; i<sortedlist.length; i++) {
+    if(sortedlist[i] === target) {
+      return i;
     }
-    return sortedlist.length;
+    else if(sortedlist[i] > target) {
+      return i--;
+    }
+
+  }
+  return sortedlist.length;
 }
 
 console.log(position([1,3,5,6], 7));
